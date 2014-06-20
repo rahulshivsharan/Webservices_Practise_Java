f16d820bf20709a24db61869c25f6be1755f8129
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-06-20 16:22:56 +0530

    Create MainTestClass.java

---
+++ src/com/service/two/MainTestClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.service.two;
+
+import com.service.two.StudentInfoImpl;
+import javax.xml.ws.Endpoint;
+
+
+public class MainTestClass {
+    public static void main(String [] args){
+        try{
+            Endpoint.publish("http://localhost:9876/go", new StudentInfoImpl());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
